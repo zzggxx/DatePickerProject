5a247c2dc50dc2140b4192c51196de617d13f84e
Will <will.g.zhou@magugi.com>
2017-11-07 19:14:20 +0800

    项目关联,日期选择控件DatePickerDialog

---
+++ DatePickerDialog/src/main/java/com/example/will/datepickerproject/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.will.datepickerproject;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * 每个系统上的显示的样式是不一样的.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int mYear, mMonth, mDay;
+    Button btnDatePickerDialog;
+    TextView dateDisplay;
+    final int DATE_DIALOG = 1;
+    private DatePickerDialog.OnDateSetListener mdateListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            display();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initViewDatePickerDialog();
+    }
+
+    private void initData() {
+        final Calendar ca = Calendar.getInstance();
+        mYear = ca.get(Calendar.YEAR);
+        mMonth = ca.get(Calendar.MONTH);
+        mDay = ca.get(Calendar.DAY_OF_MONTH);
+    }
+
+    private void initViewDatePickerDialog() {
+        btnDatePickerDialog = (Button) findViewById(R.id.dateChooseDialog);
+        dateDisplay = (TextView) findViewById(R.id.dateDisplay);
+
+        btnDatePickerDialog.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                方法已过时
+                showDialog(DATE_DIALOG);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG:
+                return new DatePickerDialog(this, mdateListener, mYear, mMonth, mDay);
+        }
+        return null;
+    }
+
+    /**
+     * 设置日期 利用StringBuffer追加
+     */
+    public void display() {
+        dateDisplay.setText(new StringBuffer().append(mMonth + 1).append("-").append(mDay).append("-").append(mYear));
+    }
+}
